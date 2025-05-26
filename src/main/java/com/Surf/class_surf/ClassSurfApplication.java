474bfe753bcf3e5964810f7d4613a3183a01e9f4
Pacheca <mpacheca7@gmail.com>
2025-05-26 11:10:39 +0200

    config

---
+++ src/main/java/com/Surf/class_surf/ClassSurfApplication.java
@@ -0,0 +1,13 @@
+package com.Surf.class_surf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassSurfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassSurfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Surf/class_surf/ClassSurfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Surf.class_surf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassSurfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
